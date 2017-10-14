abaa3fd7dbadbfeeb45017d12f5dbab4d0e29f64
Simon Smale <simon@smalemail.net>
2017-10-14 20:22:47 +0100

    Update file to include comments and enhances

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,7 @@
+//------------------//
+// Define Variables //
+//------------------//
+
 var browserSync = require('browser-sync');  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
@@ -11,30 +15,22 @@ var sourcemaps   = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var runSequence = require('run-sequence');
 
-
-// Development Tasks 
-// -----------------
+//-------------------//
+// Development Tasks //
+//-------------------//
 
 // Start browserSync server
 gulp.task('browserSync', function() {
   browserSync.init({
       server: {
           baseDir: "app"
-      },
-    tunnel: "ssmale"
+      }
+      // Tunnel has some issues currently
+      //, tunnel: "name_the_tunnel" this can be anything in lower case
   });
 });
 
-//SASS to CSS
-// gulp.task('sass', function() {
-//     gulp.src('app/scss/**/*.scss')
-//         .pipe(sass()) // Converts Sass to CSS with gulp-sass
-//         .pipe( gulp.dest('app/css') )
-//         .pipe(browserSync.reload({
-//             stream: true
-//         }));
-// });
-
+// Converts SCSS to CSS, Prefixes for the last two browser vesions and provides source maps to assit in debugging
 gulp.task('sass', function() {
    return gulp.src('app/scss/**/*.scss')
        .pipe(sourcemaps.init())
@@ -55,14 +51,18 @@ gulp.task('sass', function() {
 });
 
 // Gulp watch
-gulp.task('watch', ['browserSync', 'sass'], function (){
+gulp.task('watch', ['sass'], function (){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('app/*.html', browserSync.reload); 
   gulp.watch('app/js/**/*.js', browserSync.reload); 
 })
 
+//------------------//
+// Production Tasks //
+//------------------//
 
+// Minifies the JS and CSS files
 gulp.task('useref', function(){
   return gulp.src('app/*.html')
     .pipe(useref())
@@ -73,33 +73,26 @@ gulp.task('useref', function(){
     .pipe(gulp.dest('dist'))
 });
 
+// Moves custom fonts to the production folder
 gulp.task('fonts', function() {
   return gulp.src('app/fonts/**/*')
   .pipe(gulp.dest('dist/fonts'))
 });
 
+// Moves images to the production folder
 gulp.task('images', function() {
   return gulp.src('app/images/**/*')
   .pipe(gulp.dest('dist/images'))
 });
 
+// Deletes the production folder to remove deprecated files
 gulp.task('clean:dist', function() {
   return del.sync('dist');
 });
 
-// gulp.task('autoprefix', function () {
-//   return gulp.src('app/css')
-//     .pipe(sourcemaps.init())
-//     .pipe(postcss([ autoprefixer(
-//       {
-//         browsers: ['last 2 versions'],
-//         cascade: false
-//       }) ]))
-//     .pipe(sourcemaps.write('.'))
-//     .pipe(gulp.dest('.'));
-// });  'autoprefix',
-
-//Gulp tasks
+//------------------//
+//  Run Gulp Tasks  //
+//------------------//
 
 gulp.task('build', function (callback) {
   runSequence('clean:dist', 
@@ -109,7 +102,7 @@ gulp.task('build', function (callback) {
 });
 
 gulp.task('default', function (callback) {
-  runSequence(['sass', 'browserSync'], 'watch',
+  runSequence(['browserSync', 'sass'], 'watch',
     callback
   )
 });
